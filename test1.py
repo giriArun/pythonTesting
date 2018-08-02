ca756899b8ab54f50a7c60c4b0a0f6049cf8c751
arunpiku <giri.arun592@gmail.com>
2018-08-03 00:50:30 +0530

    Importing Modules

--- test1.py
+++ test1.py
@@ -1,11 +1,5 @@
-# Whitespace Is Important
-listofNumber = [1, 2, 3, 4, 5, 6]
+# Importing Modules
+import numpy as np
 
-for number in listofNumber:
-    print(number)
-    if (number % 2 == 0):
-        print("is even")
-    else:
-        print("is odd")
-
-print ("all done")
+a = np.random.normal(25, 5, 20)
+print(a)
