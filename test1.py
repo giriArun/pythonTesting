78b52994c6827886dc226c499eab2ee54614c4ee
arunpiku <giri.arun592@gmail.com>
2018-08-03 23:22:05 +0530

    Tuples

--- test1.py
+++ test1.py
@@ -1,24 +1,14 @@
-# Lists
+# Tuples
+#Tuples are just immutable lists. Use () instead of []
 
-x = [1, 2, 3, 4, 5, 6]
+x = (1, 2, 3)
 print(len(x))
-print(x[:3])
-print(x[3:])
-print(x[-4:])
 
-x.extend([7, 8])
-print(x)
+y = (4, 5, 6)
+print(y[2])
 
-x.append(9)
-print(x)
+listOfTuples = [x, y]
+print(listOfTuples)
 
-y = [11, 22, 33]
-listofLists = [x, y]
-print(listofLists)
-
-z = [52, 51 , 55]
-z.sort()
-print(z)
-
-z.sort(reverse=True)
-print(z)
+(age, money) = "25,12000".split(',')
+print("my age "+age+" and my salary "+money)
