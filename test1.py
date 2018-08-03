c8181d5723bc6e8a47c93492a0fed25213bcd831
arunpiku <giri.arun592@gmail.com>
2018-08-03 23:06:39 +0530

    Lists

--- test1.py
+++ test1.py
@@ -1,5 +1,24 @@
-# Importing Modules
-import numpy as np
+# Lists
 
-a = np.random.normal(25, 5, 20)
-print(a)
+x = [1, 2, 3, 4, 5, 6]
+print(len(x))
+print(x[:3])
+print(x[3:])
+print(x[-4:])
+
+x.extend([7, 8])
+print(x)
+
+x.append(9)
+print(x)
+
+y = [11, 22, 33]
+listofLists = [x, y]
+print(listofLists)
+
+z = [52, 51 , 55]
+z.sort()
+print(z)
+
+z.sort(reverse=True)
+print(z)
