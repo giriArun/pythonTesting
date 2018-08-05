4bb5d41f064c531b5b7833942662a09cadbbc6ee
arunpiku <giri.arun592@gmail.com>
2018-08-06 00:27:00 +0530

    Pandas data frames and series

---
+++ test2.py
@@ -0,0 +1,17 @@
+# matplotlib inline
+#import numpy as np
+import pandas as pd
+
+df = pd.read_csv("PastHires.csv")
+
+print(df.head())
+
+print(df.head(7))
+
+print(df.tail(3))
+
+print(df.shape)
+
+print(df.size)
+
+print(len(df))
