4f41c2b9becda4468c2429d39f075864f09b3c71
arunpiku <giri.arun592@gmail.com>
2018-08-10 23:54:16 +0530

    Pandas data frames and series part2

--- test2.py
+++ test2.py
@@ -4,14 +4,19 @@ import pandas as pd
 
 df = pd.read_csv("PastHires.csv")
 
-print(df.head())
+print("columns name of DataFrame:",df.columns)
 
-print(df.head(7))
+print("single column show from DataFrame:\n",df['Hired'])
 
-print(df.tail(3))
+print("range of rows:\n",df['Hired'][:5])
 
-print(df.shape)
+print("specified column / row:\n",df['Hired'][5])
 
-print(df.size)
+print("more then one column:\n",df[["Years Experience","Hired"]])
 
-print(len(df))
+print("specific renges of row:\n",df[['Years Experience','Hired']][:5])
+
+print("DataFrame by a specific:\n",df.sort_values(['Years Experience']))
+
+degree_counts = df['Level of Education'].value_counts()
+print("break down the number of unique values in a given column:\n",degree_counts)
